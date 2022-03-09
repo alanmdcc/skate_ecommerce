756e28e80f0a20c62d828337d41c9de37cd002a8
ManuelAguirre <manuel.aguirre.ruiz.mx@gmail.com>
2022-03-09 10:13:44 -0600

    finished card design

---
+++ js/card.js
@@ -0,0 +1,19 @@
+let card =  `
+    <div class="container">
+        <div class="row"
+
+            <div class="col-2">
+                <div class="card card-style">
+                    <img src="./57972_1800x.jpg" class="card-img-top " alt="...">
+                    <div class="card-body p-1">
+                        <div class="shop text-center">
+                            <img src="./shoppingcart.svg" alt="" class="shop-icon">
+                        </div>
+                      <p class="card-text h1 text-center">$990</p>
+                    </div>
+                  </div>
+            </div>
+
+        </div>
+    </div>
+    `;
