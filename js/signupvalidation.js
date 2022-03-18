2ab5378b8884f6e488411524527c1f4c0a7aa75b
alanmdcc <alan.martindelc@gmail.com>
2022-03-17 22:31:39 -0600

    Change validation styles in sign up page

--- js/signupvalidation.js
+++ js/signupvalidation.js
@@ -1,14 +1,7 @@
 // Validacion formulario
 const formulario = document.getElementById('form-contact'); //accedemos al bloque del formulario
-const inputs = document.querySelectorAll('#form-contact input'); // obtenemos todos los inputs de la pag
-//////////////////////////////////////////////////
-// //Elemntos del dom
-// const form = document.getElementById('form-contact');
-// const email = document.getElementById('email');
-// const phone= document.getElementById('Cellphone');
-// const person = document.getElementById('Name');
-// const message = document.getElementById('Msg');
-///////////////////////////////////////////////////
+const inputs = document.querySelectorAll('input'); // obtenemos todos los inputs de la pag
+
 const expresiones = {
 	name: /^[a-zA-ZÀ-ÿ\s]{3,40}$/, // Letras y espacios, pueden llevar acentos.
 	email: /^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$/, //resumida la linea que dejaron
@@ -23,7 +16,6 @@ const campos = {
     password: false
 }
 
-
 const validarFormularioSignup = (e) => {
     switch(e.target.name){
         case "Name":            
@@ -34,11 +26,6 @@ const validarFormularioSignup = (e) => {
             break;
         case "email":
             validarCampoSigup(expresiones.email, e.target, 'email');
-            // //funcion para validar un email, retorna true si es valido y false si no es valido
-            // function validateEmail(email){
-            //     const emailRegex = /^(([^<>()\[\]\\.,:\s@"]+(\.[^<>()\[\]\\.,:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-            //     return emailRegex.test(email);
-            // }
             break;
         
         case "password":
@@ -54,20 +41,15 @@ const validarFormularioSignup = (e) => {
 
 
 //funcion generica para validacion y cambio de clases 
-const validarCampoSigup = (expresion, input, campo) => {
+function validarCampoSigup(expresion, input, campo){
 	if(expresion.test(input.value)){
-		document.getElementById(`group--${campo}`).classList.remove('form--group-incorrecto');
-		document.getElementById(`group--${campo}`).classList.add('form--group-correcto');
-		document.querySelector(`#group--${campo} i`).classList.add('fa-check-circle');
-		document.querySelector(`#group--${campo} i`).classList.remove('fa-times-circle');
-		document.querySelector(`#group--${campo} .form--input--error`).classList.remove('form--input--error-activo');
+		document.getElementById(`${campo}`).classList.remove("is-invalid");
+		document.getElementById(`${campo}`).classList.add("is-valid");
 		campos[campo] = true;
+		console.log(campo + "cambió a true");
 	} else {
-		document.getElementById(`group--${campo}`).classList.add('form--group-incorrecto');
-		document.getElementById(`group--${campo}`).classList.remove('form--group-correcto');
-		document.querySelector(`#group--${campo} i`).classList.add('fa-times-circle');
-		document.querySelector(`#group--${campo} i`).classList.remove('fa-check-circle');
-		document.querySelector(`#group--${campo} .form--input--error`).classList.add('form--input--error-activo');
+		document.getElementById(`${campo}`).classList.add("is-invalid");
+		document.getElementById(`${campo}`).classList.remove("is-valid");
 		campos[campo] = false;
 	}
 }
@@ -77,18 +59,12 @@ const validarPassword2 = () => {
 	const inputPassword2 = document.getElementById('password2');
 
 	if(inputPassword1.value !== inputPassword2.value){
-		document.getElementById(`group--password2`).classList.add('form--group-incorrecto');
-		document.getElementById(`group--password2`).classList.remove('form--group-correcto');
-		document.querySelector(`#group--password2 i`).classList.add('fa-times-circle');
-		document.querySelector(`#group--password2 i`).classList.remove('fa-check-circle');
-		document.querySelector(`#group--password2 .form--input--error`).classList.add('form--input--error-activo');
+		document.getElementById('password2').classList.add("is-invalid");
+		document.getElementById('password2').classList.remove("is-valid");
 		campos['password'] = false;
 	} else {
-		document.getElementById(`group--password2`).classList.remove('form--group-incorrecto');
-		document.getElementById(`group--password2`).classList.add('form--group-correcto');
-		document.querySelector(`#group--password2 i`).classList.remove('fa-times-circle');
-		document.querySelector(`#group--password2 i`).classList.add('fa-check-circle');
-		document.querySelector(`#group--password2 .form--input--error`).classList.remove('form--input--error-activo');
+		document.getElementById('password2').classList.remove("is-invalid");
+		document.getElementById('password2').classList.add("is-valid");
 		campos['password'] = true;
 	}
 }
@@ -100,62 +76,30 @@ inputs.forEach((input) => {
     input.addEventListener('blur', validarFormularioSignup);
 })
 
-
-
 // cuando se presiona el boton de enviar se ejecuta un prevent default
 formulario.addEventListener('submit', (e) => {
     e.preventDefault();
 	if(campos.Name && campos.email && campos.Cellphone && campos.password){
 		formulario.reset();
 
-		document.getElementById('form--mensaje-exito').classList.add('form--mensaje-exito-activo');
+		document.getElementById('alert-success').classList.add('form--mensaje-exito-activo');
 		setTimeout(() => {
-			document.getElementById('form--mensaje-exito').classList.remove('form--mensaje-exito-activo');
+			document.getElementById('alert-success').classList.remove('form--mensaje-exito-activo');
 		}, 5000);
 
-		document.querySelectorAll('.form--group-correcto').forEach((icono) => {
-			icono.classList.remove('form--group-correcto');
+		document.querySelectorAll('.is-valid').forEach((icono) => {
+			icono.classList.remove('is-valid');
 		});
+
+		Object.keys(campos).forEach(campo => {
+			campos[campo] = false;
+		})
+
 	} else {
-		document.getElementById('form--mensaje').classList.add('form--mensaje-exito-activo');
+		document.getElementById('alert-danger').classList.add('form--mensaje-activo');
+		setTimeout(() => {
+			document.getElementById('alert-danger').classList.remove('form--mensaje-activo');
+		}, 5000);
 	}
 })
-//////////////////////////////////////////////////////////
-// //funcion para validar que todos los datotos introducidos en el formulario sea correctos
-// function validateForm(){
-//     const formValues = Object.values(formIsValid); //regresa un arreglo con todos los valores de un objeto.
-    
-//     console.log(formIsValid);
-//     //Vamos a buscar dentro del arreglo formValuer 
-//     const valid = formValues.findIndex((value) => value == false);
-
-//     if(valid === -1){
-//         form.submit(); //abre la aplicación de default de correo con la información del formulario
-//     } else{
-//         alert('El formulario es invalido');
-//     }
-// }
-//////////////////////////////////////////////////////////////
-
-
-
-// //agrego funcion para validar numero de telefono
-
-// phone.addEventListener('change', (e)=>{
-//     if (phone.value.length>10){
-//         window.alert('el número de teléfono no puede tener más de 10 digitos');
-//     }else if (phone.value.length<=0 || phone.value.length !=10){
-//         window.alert('por favor ingrese un número de teléfono válido');
-//     } else if (isNaN(phone.value)==true){
-//         window.alert('no se pueden ingresar letras en el numero de telefono');
-//     } else{
-//         formIsValid.phone = true;
-//     }
-    
-// }); 
-
-
-
-
-
 
