61f792bb9106ff859c4ec095c76261a8629cc2fa
alanmdcc <alan.martindelc@gmail.com>
2022-04-06 18:00:20 -0500

    Se agregan lista de productos a script de DB

--- DB/generatingSkateboardsDB.sql
+++ DB/generatingSkateboardsDB.sql
@@ -5,22 +5,22 @@ SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0;
 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='ONLY_FULL_GROUP_BY,STRICT_TRANS_TABLES,NO_ZERO_IN_DATE,NO_ZERO_DATE,ERROR_FOR_DIVISION_BY_ZERO,NO_ENGINE_SUBSTITUTION';
 
 -- -----------------------------------------------------
--- Schema Impuls8
+-- Schema impuls8
 -- -----------------------------------------------------
 
 -- -----------------------------------------------------
--- Schema Impuls8
+-- Schema impuls8
 -- -----------------------------------------------------
-CREATE SCHEMA IF NOT EXISTS `Impuls8` DEFAULT CHARACTER SET utf8 ;
-USE `Impuls8` ;
+CREATE SCHEMA IF NOT EXISTS `impuls8` DEFAULT CHARACTER SET utf8 ;
+USE `impuls8` ;
 
 -- -----------------------------------------------------
--- Table `Impuls8`.`Usuario`
+-- Table `impuls8`.`User`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `Impuls8`.`Usuario` (
+CREATE TABLE IF NOT EXISTS `impuls8`.`User` (
   `idUser` INT NOT NULL AUTO_INCREMENT,
   `userName` VARCHAR(45) NOT NULL,
-  `userPhone` VARCHAR(45) NULL,
+  `userPhone` VARCHAR(45) NOT NULL,
   `userEmail` VARCHAR(45) NOT NULL,
   `password` VARCHAR(256) NOT NULL,
   `isAdmin` TINYINT NOT NULL,
@@ -29,9 +29,9 @@ ENGINE = InnoDB;
 
 
 -- -----------------------------------------------------
--- Table `Impuls8`.`Category`
+-- Table `impuls8`.`Category`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `Impuls8`.`Category` (
+CREATE TABLE IF NOT EXISTS `impuls8`.`Category` (
   `idCategory` INT NOT NULL AUTO_INCREMENT,
   `categoryName` VARCHAR(45) NOT NULL,
   PRIMARY KEY (`idCategory`))
@@ -39,46 +39,46 @@ ENGINE = InnoDB;
 
 
 -- -----------------------------------------------------
--- Table `Impuls8`.`Product`
+-- Table `impuls8`.`Product`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `Impuls8`.`Product` (
+CREATE TABLE IF NOT EXISTS `impuls8`.`Product` (
   `idProduct` INT NOT NULL AUTO_INCREMENT,
   `nameProduct` VARCHAR(45) NOT NULL,
   `priceProduct` DOUBLE NOT NULL,
-  `descriptionProduct` VARCHAR(45) NOT NULL,
+  `descriptionProduct` VARCHAR(200) NOT NULL,
   `productPicture` VARCHAR(200) NOT NULL,
   `Category_idCategory` INT NOT NULL,
-  PRIMARY KEY (`idProduct`, `Category_idCategory`),
+  PRIMARY KEY (`idProduct`),
   INDEX `fk_Product_Category1_idx` (`Category_idCategory` ASC) VISIBLE,
   CONSTRAINT `fk_Product_Category1`
     FOREIGN KEY (`Category_idCategory`)
-    REFERENCES `Impuls8`.`Category` (`idCategory`)
+    REFERENCES `impuls8`.`Category` (`idCategory`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION)
 ENGINE = InnoDB;
 
 
 -- -----------------------------------------------------
--- Table `Impuls8`.`UserOrder`
+-- Table `impuls8`.`UserOrder`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `Impuls8`.`UserOrder` (
+CREATE TABLE IF NOT EXISTS `impuls8`.`UserOrder` (
   `idUserOrder` INT NOT NULL AUTO_INCREMENT,
-  `dateUserOrder` VARCHAR(45) NOT NULL,
-  `Usuario_idUser` INT NOT NULL,
+  `dateUserOrder` DATETIME NOT NULL,
+  `User_idUser` INT NOT NULL,
   PRIMARY KEY (`idUserOrder`),
-  INDEX `fk_UserOrder_Usuario1_idx` (`Usuario_idUser` ASC) VISIBLE,
-  CONSTRAINT `fk_UserOrder_Usuario1`
-    FOREIGN KEY (`Usuario_idUser`)
-    REFERENCES `Impuls8`.`Usuario` (`idUser`)
+  INDEX `fk_UserOrder_User1_idx` (`User_idUser` ASC) VISIBLE,
+  CONSTRAINT `fk_UserOrder_User1`
+    FOREIGN KEY (`User_idUser`)
+    REFERENCES `impuls8`.`User` (`idUser`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION)
 ENGINE = InnoDB;
 
 
 -- -----------------------------------------------------
--- Table `Impuls8`.`OrderProductQuantity`
+-- Table `impuls8`.`OrderProductQuantity`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `Impuls8`.`OrderProductQuantity` (
+CREATE TABLE IF NOT EXISTS `impuls8`.`OrderProductQuantity` (
   `quantity` INT NOT NULL,
   `Product_idProduct` INT NOT NULL,
   `UserOrder_idUserOrder` INT NOT NULL,
@@ -86,34 +86,34 @@ CREATE TABLE IF NOT EXISTS `Impuls8`.`OrderProductQuantity` (
   INDEX `fk_ProductQuantity_UserOrder1_idx` (`UserOrder_idUserOrder` ASC) VISIBLE,
   CONSTRAINT `fk_ProductQuantity_Product1`
     FOREIGN KEY (`Product_idProduct`)
-    REFERENCES `Impuls8`.`Product` (`idProduct`)
+    REFERENCES `impuls8`.`Product` (`idProduct`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION,
   CONSTRAINT `fk_ProductQuantity_UserOrder1`
     FOREIGN KEY (`UserOrder_idUserOrder`)
-    REFERENCES `Impuls8`.`UserOrder` (`idUserOrder`)
+    REFERENCES `impuls8`.`UserOrder` (`idUserOrder`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION)
 ENGINE = InnoDB;
 
 
 -- -----------------------------------------------------
--- Table `Impuls8`.`ProductCart`
+-- Table `impuls8`.`ProductCart`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `Impuls8`.`ProductCart` (
+CREATE TABLE IF NOT EXISTS `impuls8`.`ProductCart` (
   `quantity` INT NOT NULL,
-  `Usuario_idUser` INT NOT NULL,
+  `User_idUser` INT NOT NULL,
   `Product_idProduct` INT NOT NULL,
-  PRIMARY KEY (`Usuario_idUser`, `Product_idProduct`),
+  PRIMARY KEY (`User_idUser`, `Product_idProduct`),
   INDEX `fk_ProductCart_Product1_idx` (`Product_idProduct` ASC) VISIBLE,
-  CONSTRAINT `fk_ProductCart_Usuario1`
-    FOREIGN KEY (`Usuario_idUser`)
-    REFERENCES `Impuls8`.`Usuario` (`idUser`)
+  CONSTRAINT `fk_ProductCart_User1`
+    FOREIGN KEY (`User_idUser`)
+    REFERENCES `impuls8`.`User` (`idUser`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION,
   CONSTRAINT `fk_ProductCart_Product1`
     FOREIGN KEY (`Product_idProduct`)
-    REFERENCES `Impuls8`.`Product` (`idProduct`)
+    REFERENCES `impuls8`.`Product` (`idProduct`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION)
 ENGINE = InnoDB;
